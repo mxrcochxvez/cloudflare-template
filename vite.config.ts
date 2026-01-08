6e33b626074704efc643ee5448a4782824d9cd45
Marco Chavez <marcode.chavez.jr@gmail.com>
2026-01-07 23:25:09 -0800

    fix(config): disable v3_singleFetch to prevent SPA hydration issues

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/cloudflare" {
   interface Future {
-    v3_singleFetch: true;
+    // v3_singleFetch: true;
   }
 }
 
@@ -19,8 +19,8 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: false,
+        v3_lazyRouteDiscovery: false,
       },
     }),
     tsconfigPaths(),
